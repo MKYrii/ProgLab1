5ed57c05d6f39a699856dbc994e6b108457382b1
Uriy <mihailovywork@gmail.com>
2023-10-26 10:49:10 +0300

    Revert "добавлен новый тест"
    This reverts commit 592784f3

--- tests/lab2/test_vigenre.py
+++ tests/lab2/test_vigenre.py
@@ -1,5 +1,5 @@
 from src.lab2.vigenre import decrypt_vigenere, encrypt_vigenere
-import unittest, random, string
+import unittest
 
 
 class TestCaesar(unittest.TestCase):
@@ -19,13 +19,6 @@ class TestCaesar(unittest.TestCase):
     def test_symbols(self):
         self.assertEqual(self.decrypt_vigenere('35P654/.sar1.+', 'gahf9-'), '35J654/.stm1.+')
         self.assertEqual(self.encrypt_vigenere('35P654/.sar1.+', 'gahf9-'), '35V654/.shw1.+')
-
-    def test_randomized(self):
-        kwlen = random.randint(4, 24)
-        keyword = ''.join(random.choice(string.ascii_letters) for _ in range(kwlen))
-        plaintext = ''.join(random.choice(string.ascii_letters + ' -,') for _ in range(64))
-        ciphertext = encrypt_vigenere(plaintext, keyword)
-        self.assertEqual(plaintext, decrypt_vigenere(ciphertext, keyword))
 
 
 if __name__ == "__main__":
